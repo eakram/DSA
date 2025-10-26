aa22bda46522e3b6b6b8d0da3b006a500da597a8
MOHAMMAD AKRAM <akramakram40@gmail.com>
2025-10-26 07:46:24 +0000

    Two sum problem

---
+++ TwoSum.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+class TwoSum {
+    //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    //You can return the answer in any order.
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for(int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if(map.containsKey(comp)){
+                return new int[]{i, map.get(comp)};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+    public static void main(String[] args){
+        int []nums = {2,7,11,15};
+        int target = 9;
+        int[] output = twoSum(nums, target);
+        System.out.print("[");
+        for(int i = 0; i < output.length; i++){
+            System.out.print(output[i] + ", ");
+        }
+        System.out.println("]");
+    }
+}
